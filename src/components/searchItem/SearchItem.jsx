46f25560062c566ed2526fe518ed34b12c20560d
Mackinlay Kevin <kemack83@gmail.com>
2024-07-29 19:22:41 -0300

    working on searchItem css

---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,21 @@
+import './searchItem.css'
+
+const SearchItem = () => {
+  return (
+    <div className="searchItem">
+      <img src="https://images.pexels.com/photos/3634741/pexels-photo-3634741.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" className="siImg" />
+      <div className="siDesc">
+        <h1 className="siTitle">La nube Apartments</h1>
+        <span className="siDistance">500m from center</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">Studio Apartment with Air conditioning</span>
+        <span className="siFeatures">Entire studio • 1 bathroom • 21m² 1 full bed</span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">You can cancel later, so lock in this great price today!</span>
+      </div>
+      <div className="siDet">Details</div>
+    </div>
+  );
+}
+
+export default SearchItem
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import { format } from 'date-fns'
 import { DateRange } from 'react-date-range'
+import SearchItem from '../../components/searchItem/SearchItem'
 
 const List = () => {
 
@@ -32,11 +33,50 @@ const [options, setOptions] = useState(location.state.options)
             </div>
             <div className="lsItem">
               <label>Check-in Date</label>
-              <span onClick={()=> setOpenDate(!openDate)}>{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(date[0].endDate, 'MM/dd/yyyy')}`}</span>
-              { openDate && (<DateRange onChange={(item) => setDate([item.selection])} minDate={new Date()} ranges={date} />)}
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(date[0].endDate, 'MM/dd/yyyy')}`}</span>
+              {openDate && <DateRange onChange={(item) => setDate([item.selection])} minDate={new Date()} ranges={date} />}
             </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult </span>
+                  <input type="number" min={1} className="lsOptionInput" placeholder={options.adult} />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children </span>
+                  <input type="number" min={0} className="lsOptionInput" placeholder={options.children} />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room </span>
+                  <input type="number" min={1} className="lsOptionInput" placeholder={options.room} />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
         </div>
       </div>
     </div>
